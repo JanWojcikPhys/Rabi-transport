6eae295396be36e672b41d4ee72642d436e2c720
Jan Wójcik <118134128+JanWojcikPhys@users.noreply.github.com>
2025-08-19 10:09:04 +0200

    Update

--- data_fig5.jl
+++ data_fig5.jl
@@ -14,7 +14,7 @@ c = cos(π/40)
 s = sin(π/40)
 c2 = cos(π/80)
 s2 = sin(π/80)
-function ewolucja(d,tmax)                               # tmax - ostatni czasowy element 
+function ewolucja(d,tmax)                               
     ss = S(d);
     data = spzeros(Float64,tmax,d);
     psi = psi0(d);
